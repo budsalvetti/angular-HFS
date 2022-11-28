3a36d38a6ea27efff13de27fd590d58c7af307e9
budsalvetti <budsalvetti@gmail.com>
2022-11-28 12:43:53 -0800

    Now using child views and initializing afterViewInit with variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,38 @@
 import '@grapecity/wijmo.styles/wijmo.css';
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
 import * as wjGrid from '@grapecity/wijmo.grid';
 import * as wjcSheet from '@grapecity/wijmo.grid.sheet';
+import { WjFlexSheet } from '@grapecity/wijmo.angular2.grid.sheet';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  initializeFlexSheet(flex: wjcSheet.FlexSheet) {
-      // init grid
+export class AppComponent implements AfterViewInit {
+
+  @ViewChild(WjFlexSheet) flex;
+  @ViewChild('flex_sheet') flexSheet;
+
+  readonly numRows = 11;
+  readonly numColumns = 6;
+  readonly sheetName = "Next Gen Worksheet";
+
+  initializeFlexSheet() {
+    
+    this.flexSheet.rowCount = this.numRows;
+    this.flexSheet.columnCount = this.numColumns;
+    this.flexSheet.name = "Next Gen Worksheet";
+
+      for (let row = 0; row < this.numRows; row++) {
+          for (let col =  0; col < this.numColumns; col++) {
+              this.flex.setCellData(row, col, row + col);
+          }
+      }
+  }
+
+  ngAfterViewInit(){
+    this.initializeFlexSheet()
   }
    
 }
